74b539ebd49e2e5a3581fd382765ba9b6cad2a17
yk1n0rt <yk1n0rt@gmail.com>
2023-10-13 08:53:45 +0800

    inall

--- app/src/main/java/com/fongmi/android/tv/impl/FilterCallback.java
+++
@@ -1,6 +0,0 @@
-package com.fongmi.android.tv.impl;
-
-public interface FilterCallback {
-
-    void setFilter(String key, String value);
-}
--- forcetech/src/main/java/com/gsoft/mitv/LocalBinder.java
+++
@@ -1,6 +0,0 @@
-package com.gsoft.mitv;
-
-import android.os.Binder;
-
-public class LocalBinder extends Binder {
-}
